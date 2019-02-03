5d5bbb2e23818ad90b24d0a96027f85f5b30aa84
Javi <nhitbh@gmail.com>
2019-02-03 20:25:12 +0100

    Added code to handle server requests and establish a new websocket connection

--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
+	"github.com/gorilla/websocket"
+)
+
+const (
+	API     string = "/api"
+	VERSION string = "v0.1.0"
+	BANNER  string = `
+        __  .__              __          __          
+  _____/  |_|  |__   _______/  |______ _/  |_  ______
+_/ __ \   __\  |  \ /  ___/\   __\__  \\   __\/  ___/
+\  ___/|  | |   Y  \\___ \  |  |  / __ \|  |  \___ \ 
+ \___  >__| |___|  /____  > |__| (____  /__| /____  >
+     \/          \/     \/            \/          \/  %s
+`
+)
+
+var (
+	addr = flag.String("addr", "localhost:3000", "http service address")
+)
+
+// upgradeConnection allows
+var upgradeConnection = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return strings.Compare(r.RequestURI, API) == 0
+	},
+	Subprotocols: []string{"ws", "wss"},
+}
+
+// handleRequest is the function to handle all server requests...
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	c, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgradeConnection:", err)
+		return
+	}
+	defer c.Close()
+
+	// Server loop
+	for {
+		_, _, err := c.ReadMessage()
+		if err != nil {
+			// Connection close by the origin
+			break
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	fmt.Printf(BANNER, VERSION)
+	log.Printf("Starting server on %s", *addr)
+	http.HandleFunc(API, handleRequest)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
