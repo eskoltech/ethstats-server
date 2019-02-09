0977326e394e5777939cf29888a8c87c99704ad5
Javi <nhitbh@gmail.com>
2019-02-09 11:37:56 +0100

    Removed unused struct in message/detail.go

--- message/detail.go
+++ message/detail.go
@@ -6,25 +6,10 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// NodeInfo is the information related to the Ethereum node
-type NodeInfo struct {
-	Name     string `json:"name"`
-	Node     string `json:"node"`
-	Port     int    `json:"port"`
-	Network  string `json:"net"`
-	Protocol string `json:"protocol"`
-	API      string `json:"api"`
-	Os       string `json:"os"`
-	OsVer    string `json:"os_v"`
-	Client   string `json:"client"`
-	History  bool   `json:"canUpdateHistory"`
-}
-
 // AuthMessage is the struct sent by the server on the first connection
 type AuthMessage struct {
-	ID     string   `json:"id"`
-	Secret string   `json:"secret"`
-	Info   NodeInfo `json:"info"`
+	ID     string `json:"id"`
+	Secret string `json:"secret"`
 }
 
 // SendResponse send the ready response to the node to initiate the communication
