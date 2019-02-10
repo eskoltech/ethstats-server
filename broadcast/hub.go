3d0794d786fa4ecf1ee56eab3ce58074be94fe38
Javi <nhitbh@gmail.com>
2019-02-10 15:43:28 +0100

    A new loop was created to register new clients and broadcast messages to it when the server starts

---
+++ broadcast/hub.go
@@ -0,0 +1,44 @@
+package broadcast
+
+import (
+	"github.com/eskoltech/ethstats/service"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+// hub maintain a list of registered clients to send messages
+type hub struct {
+	register chan *websocket.Conn
+	clients  map[*websocket.Conn]bool
+	service  *service.Channel
+}
+
+// loop loops as the server is alive and send messages to registered clients
+func (h *hub) loop() {
+	for {
+		select {
+		case msg := <-h.service.Message:
+			if len(h.clients) == 0 {
+				continue
+			}
+			h.writeMessage(msg)
+		case client := <-h.register:
+			h.clients[client] = true
+		}
+	}
+}
+
+// writeMessage to all registered clients. If an error occurs sending a message to a client,
+// then these connection is closed and removed from the pool of registered clients
+func (h *hub) writeMessage(msg []byte) {
+	for client := range h.clients {
+		err := client.WriteMessage(1, msg)
+		if err != nil {
+			log.Infof("Closed connection with client: %s", client.RemoteAddr())
+
+			// close and delete the client connection and release
+			client.Close()
+			delete(h.clients, client)
+		}
+	}
+}
---
+++ broadcast/server.go
@@ -0,0 +1,48 @@
+package broadcast
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/eskoltech/ethstats/service"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+// Root is the home endpoint where hub are registered to receive node updates
+const Root string = "/"
+
+// upgradeConnection upgrade only HTTP request to the / endpoint
+var upgradeConnection = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return strings.Compare(r.RequestURI, Root) == 0
+	},
+}
+
+// Server is the responsible to send node state to registered hub
+type Server struct {
+	hub *hub
+}
+
+// New creates a new Server struct with the required service
+func New(service *service.Channel) *Server {
+	hub := &hub{
+		register: make(chan *websocket.Conn),
+		clients:  make(map[*websocket.Conn]bool),
+		service:  service,
+	}
+	go hub.loop()
+	return &Server{hub: hub}
+}
+
+// HandleRequest handle all request from hub that are not Ethereum nodes
+func (s *Server) HandleRequest(w http.ResponseWriter, r *http.Request) {
+	clientConn, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Errorf("Error trying to establish communication with client (addr=%s, host=%s, URI=%s), %s",
+			r.RemoteAddr, r.Host, r.RequestURI, err)
+		return
+	}
+	log.Infof("Connected new client! (host=%s)", r.Host)
+	s.hub.register <- clientConn
+}
